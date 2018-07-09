e4e3cec9b72276890150c18699ff9eb9073588db
Chaos <ecidevilin@live.cn>
2018-07-09 13:54:22 +0800

    base class

---
+++ Heap.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Heap<T> where T : IComparable<T>
+{
+    protected List<T> _A;
+
+    public delegate bool Compare<T>(T a, T b);
+
+    protected Compare<T> _compareFunc;
+
+    public Heap(Compare<T> _func)
+    {
+        _compareFunc = _func;
+        _A = new List<T>();
+    }
+
+    public Heap(Compare<T> _func, List<T> a)
+    {
+        _compareFunc = _func;
+        Build(a);
+    }
+    public static int Parent(int idx)
+    {
+        return (idx - 1) / 2;
+    }
+
+    public static int Left(int idx)
+    {
+        return 2 * idx + 1;
+    }
+
+    public static int Right(int idx)
+    {
+        return 2 * idx + 2;
+    }
+    public void Swap(int i, int j)
+    {
+        T t = _A[i];
+        _A[i] = _A[j];
+        _A[j] = t;
+    }
+
+    public int Size()
+    {
+        return _A.Count;
+    }
+
+    public T Top()
+    {
+        return _A[0];
+    }
+
+    public virtual T ExtractTop()
+    {
+        if (_A.Count == 0)
+        {
+            throw new Exception("Heap underflow");
+        }
+        T top = _A[0];
+        int last = _A.Count - 1;
+        _A[0] = _A[last];
+        _A.RemoveAt(last);
+        Heapify(0);
+        return top;
+    }
+
+    public virtual void Insert(T key)
+    {
+
+        _A.Add(key);
+        ModifyKey(_A.Count - 1, key);
+    }
+
+    public virtual void Build(List<T> a)
+    {
+        _A = a;
+        for (int i = a.Count / 2; i >= 0; i--)
+        {
+            Heapify(i);
+        }
+    }
+
+    public virtual void Heapify(int i)
+    {
+        int l = Left(i);
+        int r = Right(i);
+        int m = i;
+        int size = _A.Count;
+        if (l < size && _compareFunc(_A[l], _A[i]))
+        {
+            m = l;
+        }
+        if (r < size && _compareFunc(_A[r], _A[m]))
+        {
+            m = r;
+        }
+        if (m != i)
+        {
+            Swap(i, m);
+            Heapify(m);
+        }
+    }
+
+    public virtual void ModifyKey(int i, T key)
+    {
+
+        if (_compareFunc(_A[i], key))
+        {
+            throw new Exception("New key invalid");
+        }
+        _A[i] = key;
+        int p = Parent(i);
+        while (i > 0 && _compareFunc(_A[i], _A[p]))
+        {
+            Swap(i, p);
+            i = p;
+            p = Parent(i);
+        }
+    }
+}
--- MaxHeap.cs
+++ MaxHeap.cs
@@ -2,113 +2,18 @@
 using System.Collections.Generic;
 
 
-public class MaxHeap<T> where T : IComparable<T>
+public class MaxHeap<T> : Heap<T> where T : IComparable<T>
 {
-    private List<T> _A;
-    public MaxHeap()
+    public MaxHeap() : base(func)
     {
-        _A = new List<T>();
+
+    }
+    public MaxHeap(List<T> a) : base(func, a)
+    {
     }
 
-    public MaxHeap(List<T> a)
+    private static bool func(T a, T b)
     {
-        BuildMaxHeap(a);
-    } 
-    public static int Parent(int idx)
-    {
-        return (idx - 1) / 2;
-    }
-
-    public static int Left(int idx)
-    {
-        return 2 * idx + 1;
-    }
-
-    public static int Right(int idx)
-    {
-        return 2 * idx + 2;
-    }
-
-    public void MaxHeapify(int i)
-    {
-        int l = Left(i);
-        int r = Right(i);
-        int max = i;
-        int size = _A.Count;
-        if (l < size && _A[l].CompareTo(_A[i]) > 0)
-        {
-            max = l;
-        }
-        if (r < size && _A[r].CompareTo(_A[max]) > 0)
-        {
-            max = r;
-        }
-        if (max != i)
-        {
-            Swap(i, max);
-            MaxHeapify(max);
-        }
-    }
-
-    public void Swap(int i, int j)
-    {
-        T t = _A[i];
-        _A[i] = _A[j];
-        _A[j] = t;
-    }
-
-    public void BuildMaxHeap(List<T> a)
-    {
-        _A = a;
-        for (int i = a.Count / 2; i >= 0; i--)
-        {
-            MaxHeapify(i);
-        }
-    }
-
-    public T HeapMaximun()
-    {
-        return _A[0];
-    }
-
-    public int Size()
-    {
-        return _A.Count;
-    }
-
-    public T HeapExtractMax()
-    {
-        if (_A.Count == 0)
-        {
-            throw new Exception("Heap underflow");
-        }
-        T max = _A[0];
-        int last = _A.Count - 1;
-        _A[0] = _A[last];
-        _A.RemoveAt(last);
-        MaxHeapify(0);
-        return max;
-    }
-
-    public void HeapIncreaseKey(int i, T key)
-    {
-        if (key.CompareTo(_A[i]) < 0)
-        {
-            throw new Exception("New key is smaller than current key");
-        }
-        _A[i] = key;
-        int p = Parent(i);
-        while (i > 0 && _A[p].CompareTo(_A[i]) < 0)
-        {
-            Swap(i, p);
-            i = p;
-            p = Parent(i);
-        }
-    }
-
-    public void HeapInsert(T key)
-    {
-        _A.Add(key);
-        HeapIncreaseKey(_A.Count - 1, key);
+        return a.CompareTo(b) > 0;
     }
 }
