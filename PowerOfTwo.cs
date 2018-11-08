c2390010babbf272e9cb728fbf584529b65e9eb2
Chaos <ecidevilin@live.cn>
2018-11-08 20:03:43 +0800

    pot

---
+++ PowerOfTwo.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Chaos
+{
+    public static class PowerOfTwo
+    {
+        public static int ToLarger(int n)
+        {
+            if ((n & (n - 1)) != 0)
+            {
+                n |= (n >> 1);
+                n |= (n >> 2);
+                n |= (n >> 4);
+                n |= (n >> 8);
+                n |= (n >> 16);
+                n++;
+                if (n < 0)
+                {
+                    n >>= 1;
+                }
+            }
+            return n;
+        }
+    }
+}
